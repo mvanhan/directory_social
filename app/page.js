787481d4237a6d5894095fa7b7d765b032a965ef
Miller Van Hanken <mvanhan@unc.edu>
2023-12-24 12:02:03 -0600

    add

--- app/page.js
+++ app/page.js
@@ -1,113 +1,120 @@
-import Image from 'next/image'
+"use client"
+import Color from "@/components/color";
+import Coolness from "@/components/coolness"
+import Opium from "@/components/opium";
+import {opiumOutput} from "./contexts"
+import { useState } from "react";
+
 
 export default function Home() {
+  const coolnessState = useState(false)
+  const colorState = useState(false)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div>
+      <nav className="nav">
+        <a href="#about" className="nav-link">
+          <h1>ABOUT</h1>
+        </a>
+        <a href="#products" className="nav-link">
+          <h1>THEMES</h1>
+        </a>
+        <a href="#identify" className="nav-link">
+          <h1>IDENTIFY</h1>
+        </a>
+      </nav>
+      <br />
+      <br />
+      <div className="head-box" style={{ fontSize: '30px' }}>
+        <h1>Welcome to Directory-Social</h1>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="flexing" style={{ marginTop: '40px' }}>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzOOBnfnOFn5Yr4eUcPAS88NjiHXcQBxh_Ug&usqp=CAU"
+          alt="Image 1"
+        />
+        {/* <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbm5ngswFVj4_7kPrA4629ijSac1xHqP4X-Q&usqp=CAU"
+          alt="Image 2"
+        /> */}
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2Ighvdq_W5oEmM17Dfh-tPw8LRXJZyVdD4A&usqp=CAU"
+          alt="Image 3"
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <section id="about" className="content">
+        <h2 style={{ textAlign: 'center' }}>ABOUT</h2>
+        <div className="box">
+          <div className="pad">
+            <div className="flexing">
+              <div className="small-box">
+                <h1 style={{ fontSize: '25px' }}>DIRECTORY</h1>
+              </div>
+              <div className="spacing"></div>
+              <div className="small-box">
+                <h1 style={{ fontSize: '25px' }}>SOCIAL</h1>
+              </div>
+            </div>
+            <br />
+            <p
+              style={{
+                fontSize: '25px',
+                textAlign: 'center',
+                fontFamily: 'sans-serif',
+                fontWeight: 'bold',
+              }}
+            >
+              Directory-Social is the name of a dumb organization filled with people who think they are cooler than they are. The members of this organization subscribe to a way of life known as Opium. As defined by the Urban Dictionary, Opium is "edgy, emo, and very playboi carti core." I, myself, am a member of Directory-Social. Am I Opium? Well, I'll leave that up to you to decide. Thank you for visiting this directory.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section id="products" className="content">
+        <h2 style={{ textAlign: 'center' }}>THEMES</h2>
+        <div className="box">
+          <div className="pad">
+            <div className="flexing" style={{ fontSize: '32px' }}>
+              <ul>
+                <li>culture</li>
+                <li>creativity</li>
+                <li>civilization</li>
+                <li>experience</li>
+                <li>fashion</li>
+              </ul>
+              <div className="spacing"></div>
+              <ul>
+                <li>ability</li>
+                <li>develop</li>
+                <li>education</li>
+                <li>progress</li>
+                <li>advance</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="identify" className="content">
+        <h2 style={{ textAlign: 'center' }}>IDENTIFY</h2>
+        <div className="box">
+          <div className="pad">
+            <div style={ {textAlign: "center", fontSize: "35px"} }>Are you Opium?</div>
+            <br />
+            <br />
+            <opiumOutput.Provider value={[coolnessState, colorState]}>
+              <div className="flexing">
+                <div className="small-box">
+                  <Coolness />
+                </div>
+                <div className="spacing">
+                  <Opium />
+                </div>
+                <div className="small-box">
+                  <Color />
+                </div>
+              </div>
+            </opiumOutput.Provider>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
